ed024d9a4df0549b52249d92a4f7a418ef89ec0e
Mirko Donnarumma <mirko.donnarumma@outlook.it>
2025-11-22 17:34:40 +0100

    hotfix: more trial and error #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@components": path.resolve("./src/components"),
+        "@components": path.resolve(__dirname, "src/components"),
       },
     },
   },
